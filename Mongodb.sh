8a19d21bc625b3060b7b1f4c1cd39ef91404315f
vardhan <snucy123@gmail.com>
2026-03-14 15:35:30 +0530

    Hello world

--- Mongodb.sh
+++ Mongodb.sh
@@ -1,59 +1,26 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #This creates a unique log file per script. [/var/log/shellscript-logs/16-logs.log]
+source ./common.sh
+app_name=mongodb
 
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-#TO CHECK USER IS IN ROOT ACCESS OR NOT
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
-
-# VALIDATE FUNCTION TO INSTALL PACKAGES.
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e " $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e " $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+check_root
 
 cp mongo.repo /etc/yum.repos.d/mongodb.repo
-VALIDATE $? "COPYING MONGODB REPO DATA"
-dnf install mongodb-org -y  &>>$LOG_FILE
-VALIDATE $? "MONGODB INSTALLATION  "
+VALIDATE $? "Copying MongoDB repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb server"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE "$?" "MONGODB ENABLEING"
+VALIDATE $? "Enabling MongoDB"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE "$?" "STARTING MONGODB"
+VALIDATE $? "Starting MongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf # here we use this as vim editor here what we are doing is changing port numbers in /etc/mongod.cong file.
-
-VALIDATE "$?" "CHANGED PORT NUMBERS IN MONGODB CONF FILE"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE "$?" "MONGODB RESTARTED SUCESSFULLY"
+VALIDATE $? "Restarting MongoDB"
 
-
-
-
-
-
+print_time
